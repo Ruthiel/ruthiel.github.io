c7b51c91c37c5bf3c1aa652b0d51995a12ee70ed
Ruthiel <ruthiel.trevisan@gmail.com>
2016-03-30 22:38:43 +0100

    Added files via upload

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute ('src','images/ruthiel.JPG');
+    } else {
+      myImage.setAttribute ('src','images/IMG_2597.JPG');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h4');
+
+function setUserName() {
+  var myName = prompt('Por favor, escreva o seu nome.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Bem-vind@ ' + myName + "!!!";
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Bem-vind@ de volta ' + storedName + "!!!";
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
